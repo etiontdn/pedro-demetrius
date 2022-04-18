5503fdd0262ef22a44cad2ef8c628035d799d657
João J <etiontdn@gmail.com>
2022-04-18 17:20:49 +0000

    Made Header

---
+++ components/Photo.vue
@@ -0,0 +1,10 @@
+<template>
+    <figure class="h-full">
+        <img class="h-full" src="@/assets/images/Foto.png" alt="Foto Pedro Demetrius">
+    </figure>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,1 +1,3 @@
-<template><div>Header</div></template>
+<template>
+  <header class="px-3 mt-2 mb-2 flex justify-between h-[60px]"><Title></Title><Photo></Photo></header>
+</template>
