ffc6d47c85771fcb8aebbb9eff0770792f47a607
Pão <bread@pop-os.localdomain>
2022-06-15 17:59:30 -0300

    Added netlify CMS

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,17 @@
 <script>
 export default {
   name: 'Inicial',
+
+  mounted() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }
 }
 </script>
