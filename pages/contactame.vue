96b269b3886d37dc552aa7ef10466ba1639a5999
etiontdn <bread@pop-os.localdomain>
2022-06-16 18:45:07 -0300

    Finished @nuxt/content config 2

--- pages/contactame.vue
+++ pages/contactame.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col gap-4 px-3">
     <div class="nav-head flex items-center">
-      <a class="go-back -translate-y-1" href="#" @click="goToPrev">
+      <NuxtLink to="/" class="go-back -translate-y-1">
         <Icon icon="chevron-right" class="h-[2.25rem] w-[2.25rem] scale-[-1]"></Icon>
-      </a>
+      </NuxtLink>
       <h1 class="title">Quien soy</h1>
     </div>
     <div class="center-photo flex justify-center">
