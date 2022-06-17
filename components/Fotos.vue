80f3b242050344c41c299eb372c01ee965288aa4
etiontdn <etiontdn@gmail.com>
2022-06-17 14:51:34 -0300

    fix netlify cms config

--- components/Fotos.vue
+++ components/Fotos.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <Categoria v-visible="image" link="/archivo/fotos" :catImg="image">Fotos</Categoria>
-        <p class="text px-3 pt-1"></p>
+        <p class="text px-3 pt-1">
+            {{ description }}
+        </p>
     </div>
 </template>
 
@@ -9,13 +11,15 @@
 export default {
     data() {
         return {
-            image: false
+            image: false,
+            description: ''
         }
     },
 
     async fetch() {
         const thumbnail = await this.$content("thumbnails").sortBy('date', 'desc').limit(1).fetch()
         this.image = thumbnail[0].fotos
+        this.description = thumbnail[0].description_fotos
     },
 
     fetchKey: 'categoria-fotos',
